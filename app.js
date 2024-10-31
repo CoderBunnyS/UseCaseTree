c8052dc3dba630d0e87d18ec85e4c8b92c91354d
bunnyfusionauth <bunny.schaefer@fusionauth.io>
2024-10-31 15:09:28 -0600

    updated links to use cases

--- app.js
+++ app.js
@@ -42,55 +42,119 @@ const questions = [
   ];
   
   const useCases = {
-    "App Suite": {
-      description: "Seamlessly transition users across applications in a suite with unified login.",
-      diagrams: ["Cross-app SSO flow"],
-      documentationLink: "https://fusionauth.io/docs/app-suite"
-    },
     "Auth as a Service": {
-      description: "Easily add authentication with configuration-based features (e.g., social logins, MFA, passkeys).",
-      diagrams: ["Authentication flow", "MFA options", "Token management"],
-      documentationLink: "https://fusionauth.io/docs/auth-as-a-service"
+      description: `
+        Easily add authentication with configuration-based features, making it simple to integrate modern auth requirements with minimal code.
+        <ul>
+          <li>Choose from multiple <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/#social-identity-providers" target="_blank">social login providers</a> like Google, Facebook, and more for a streamlined experience.</li>
+          <li>Secure accounts with <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">multi-factor authentication (MFA)</a> using SMS, TOTP, or email-based authentication.</li>
+          <li>Offer passwordless login via <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">one-time passwords (OTP)</a> or <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/magic-links" target="_blank">magic links</a> sent to user devices.</li>
+          <li>Support <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/webauthn-passkeys" target="_blank">passkeys and biometric login</a> for secure, passwordless authentication.</li>
+          <li>Enable <a href="https://fusionauth.io/docs/lifecycle/register-users/basic-registration-forms" target="_blank">self-service registration</a> with customizable forms.</li>
+          <li>Allow users to manage their profiles with <a href="https://fusionauth.io/docs/lifecycle/manage-users/account-management/" target="_blank">self-service profile management</a>.</li>
+          <li>Verify user accounts with <a href="https://fusionauth.io/docs/lifecycle/manage-users/verification/registration-gate-accounts-until-verified" target="_blank">email-based verification</a> to ensure account ownership.</li>
+        </ul>
+      `,
+    },
+    "App Suite": {
+      description: `
+        Seamlessly transition users across applications in a suite with unified login and Single Sign-On (SSO) capabilities.
+        <ul>
+          <li>Choose from multiple <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/#social-identity-providers" target="_blank">social login providers</a> like Google, Facebook, and more for a streamlined experience.</li>
+          <li>Secure accounts with <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">multi-factor authentication (MFA)</a> using SMS, TOTP, or email-based authentication.</li>
+          <li>Offer passwordless login via <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">one-time passwords (OTP)</a> or <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/magic-links" target="_blank">magic links</a> sent to user devices.</li>
+          <li>Support <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/webauthn-passkeys" target="_blank">passkeys and biometric login</a> for secure, passwordless authentication.</li>
+          <li>Enable <a href="https://fusionauth.io/docs/lifecycle/register-users/basic-registration-forms" target="_blank">self-service registration</a> with customizable forms.</li>
+          <li>Allow users to manage their profiles with <a href="https://fusionauth.io/docs/lifecycle/manage-users/account-management/" target="_blank">self-service profile management</a>.</li>
+          <li>Verify user accounts with <a href="https://fusionauth.io/docs/lifecycle/manage-users/verification/registration-gate-accounts-until-verified" target="_blank">email-based verification</a> to ensure account ownership.</li>
+          <li>Seamlessly integrate <a href="https://fusionauth.io/articles/authentication/combine-sso-mfa-fusionauth#single-sign-on" target="_blank">Single Sign-On (SSO)</a> across multiple applications in your suite.</li>
+        </ul>
+      `,
     },
     "Business to Business to Consumer (B2B2C)": {
-      description: "Offer a SaaS package where customers have consumers as end-users.",
-      diagrams: ["Organization-specific login", "Permissions schema"],
-      documentationLink: "https://fusionauth.io/docs/b2b2c"
-    },
+        description: `
+          Offer a SaaS package where customers have consumers as end-users, with advanced configuration options for social logins, multi-factor authentication, passwordless options, and more.
+          <ul>
+            <li>Choose from multiple <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/#social-identity-providers" target="_blank">social login providers</a> like Google, Facebook, and more for a streamlined experience.</li>
+            <li>Secure accounts with <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">multi-factor authentication (MFA)</a> using SMS, TOTP, or email-based authentication.</li>
+            <li>Offer passwordless login via <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/multi-factor-authentication#overview" target="_blank">one-time passwords (OTP)</a> or <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/magic-links" target="_blank">magic links</a> sent to user devices.</li>
+            <li>Support <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/passwordless/webauthn-passkeys" target="_blank">passkeys and biometric login</a> for secure, passwordless authentication.</li>
+            <li>Enable <a href="https://fusionauth.io/docs/lifecycle/register-users/basic-registration-forms" target="_blank">self-service registration</a> with customizable forms.</li>
+            <li>Allow users to manage their profiles with <a href="https://fusionauth.io/docs/lifecycle/manage-users/account-management/" target="_blank">self-service profile management</a>.</li>
+            <li>Verify user accounts with <a href="https://fusionauth.io/docs/lifecycle/manage-users/verification/registration-gate-accounts-until-verified" target="_blank">email-based verification</a> to ensure account ownership.</li>
+            <li>Seamlessly integrate <a href="https://fusionauth.io/articles/authentication/combine-sso-mfa-fusionauth#single-sign-on" target="_blank">Single Sign-On (SSO)</a> across multiple applications in your suite.</li>
+            <li><strong>User Isolation/Segmentation</strong>: Separate users by client organization to ensure <a href="https://fusionauth.io/docs/extend/examples/multi-tenant#why-use-multi-tenancy" target="_blank">data isolation and enhanced security</a> for each business.</li>
+            <li><strong>Organization/Customer-specific Password and Auth Rules</strong>: Set distinct <a href="https://fusionauth.io/docs/get-started/core-concepts/tenants#tenant-configuration" target="_blank">password and authentication policies</a> per client organization for security and compliance.</li>
+          </ul>
+        `,
+        documentationLink: "https://fusionauth.io/docs/b2b2c"
+      },
     "Business to Business to Employee (B2B2E)": {
-      description: "SaaS solution accessed by customer employees, often managed via third-party directories.",
-      diagrams: ["SCIM", "SAML"],
-      documentationLink: "https://fusionauth.io/docs/b2b2e"
-    },
-    "Machine to Machine Communication (M2M)": {
-      description: "Secure inter-application communication using client credentials and access tokens.",
-      diagrams: ["M2M authentication flow", "Token management"],
-      documentationLink: "https://fusionauth.io/docs/m2m"
-    },
-    "Identity Broker": {
-      description: "Serve as an identity broker within a private cloud or data center for customer-specific IdPs.",
-      diagrams: ["Identity provider integration flow"],
-      documentationLink: "https://fusionauth.io/docs/identity-broker"
-    },
-    "Authorization Hub for Social": {
-      description: "Centralize third-party API tokens for social providers like Google and Facebook.",
-      diagrams: ["Token lifecycle and storage"],
-      documentationLink: "https://fusionauth.io/docs/authorization-hub-for-social"
-    },
-    "API User Consents and Delegated Access": {
-      description: "Enable users to delegate API access via OAuth grants.",
-      diagrams: ["OAuth grant and consent flow"],
-      documentationLink: "https://fusionauth.io/docs/api-user-consents"
-    },
-    "Data Store": {
-      description: "Use FusionAuth as a user data store with custom login flows and UX control.",
-      diagrams: ["Custom workflows"],
-      documentationLink: "https://fusionauth.io/docs/data-store"
-    }
+        description: `
+          Provide secure, customizable access for employees of other businesses with advanced capabilities tailored for enterprise needs, such as segmentation, SSO, and custom domains.
+      <ul>
+        <li><strong>User Isolation/Segmentation</strong>: Separate users by client organization to ensure <a href="https://fusionauth.io/docs/extend/examples/multi-tenant#why-use-multi-tenancy" target="_blank">data isolation and enhanced security</a> for each business.</li>
+        <li><strong>Organization/Customer-specific Password and Auth Rules</strong>: Set distinct <a href="https://fusionauth.io/docs/get-started/core-concepts/tenants#tenant-configuration" target="_blank">password and authentication policies</a> per client organization for security and compliance.</li>
+        <li><strong>Flexible Identity Federation</strong>: Integrate with third-party <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers" target="_blank">identity providers</a>, allowing organizations to bring their own IdP or <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/overview-samlv2" target="_blank">federate identities seamlessly</a>.</li>
+        <li><strong>Permissions and Role Mapping</strong>: Map <a href="https://fusionauth.io/docs/get-started/core-concepts/roles" target="_blank">roles</a> and <a href="https://fusionauth.io/articles/identity-basics/authorization-models#rbac-simplifying-access-management" target="_blank">permissions</a> for each organization, providing precise access control for various employee roles.</li>
+        <li><strong>Single Sign-On (SSO)</strong>: Enable <a href="https://fusionauth.io/articles/authentication/combine-sso-mfa-fusionauth#single-sign-on" target="_blank">SSO across multiple applications and domains</a> within the same client organization to create a seamless experience.</li>
+        <li><strong>Branding and White Labeling</strong>: Customize login screens, emails, and UI elements to reflect each client’s brand identity, providing a <a href="https://fusionauth.io/blog/private-labeling-with-multi-tenant" target="_blank">consistent user experience</a>.</li>
+        <li><strong>SAML</strong>: Support <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/#saml" target="_blank">SAML for enterprise-grade federation</a> and easy integration with existing client identity systems.</li>
+        <li><strong>Custom Domains</strong>: Use <a href="https://fusionauth.io/blog/introducing-unlimited-custom-domains-for-fusionauth-cloud" target="_blank">client-specific domains</a> (e.g., customerA.yourplatform.com) to offer a branded experience.</li>
+        <li><strong>Entities</strong>: Leverage <a href=" https://fusionauth.io/docs/extend/examples/modeling-organizations" target="_blank">entities to represent external users, organizations, or devices</a>, allowing complex relationship management and access control.</li>
+      </ul>
+        `,
+      },
+      "Machine to Machine Communication (M2M)": {
+        description: `
+          Secure inter-application communication using client credentials and access tokens, designed to handle automated processes and system interactions without human intervention.
+          <ul>
+            <li><strong>Entity Management</strong>: Manage entities like applications, devices, or services with <a href="https://fusionauth.io/docs/get-started/core-concepts/entity-management" target="_blank">entity management</a> for better control over machine identities.</li>
+            <li><strong>Machine-to-Machine Authorization</strong>: Enable secure, token-based communication between applications using <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/oauth/modes#machine-to-machine-authorization" target="_blank">machine-to-machine authorization</a> with OAuth client credentials.</li>
+            <li><strong>Client Credentials Grant Example</strong>: See an <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/oauth/#example-client-credentials-grant" target="_blank">example implementation of the client credentials grant</a> to better understand the setup process.</li>
+          </ul>
+        `,
+      },
+      "Identity Broker": {
+        description: `
+          Serve as an identity broker within a private cloud or data center for customer-specific IdPs, streamlining authentication across multiple identity providers.
+          <ul>
+            <li><strong>Third-Party Login and Registration</strong>: Facilitate seamless <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/oauth/modes#third-party-login-and-registration" target="_blank">third-party login and registration</a> through an identity broker, allowing users to authenticate with various identity providers.</li>
+          </ul>
+        `,
+      },
+      "Authorization Hub for Social": {
+        description: `
+          Centralize third-party API tokens for social providers like Google and Facebook, allowing users to authenticate easily and manage linked accounts.
+          <ul>
+            <li><strong>Social Identity Providers</strong>: Support multiple <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/identity-providers/#social-identity-providers" target="_blank">social identity providers</a> for streamlined user authentication.</li>
+            <li><strong>Link Social Media Accounts</strong>: Allow users to <a href="https://fusionauth.io/docs/apis/identity-providers/links" target="_blank">link their social media accounts</a> to their profiles for simplified, centralized access management.</li>
+          </ul>
+        `,
+      },
+      "API User Consents and Delegated Access": {
+        description: `
+          Enable users to delegate API access via OAuth grants, allowing granular control over data access and permissions.
+          <ul>
+            <li><strong>Custom Scopes</strong>: Define <a href="https://fusionauth.io/blog/custom-scopes-in-third-party-applications" target="_blank">custom scopes for third-party applications</a> to specify the exact permissions granted to external apps.</li>
+            <li><strong>Scope Management</strong>: Manage OAuth scopes effectively to control what data users can access. See <a href="https://fusionauth.io/docs/lifecycle/authenticate-users/oauth/scopes#overview" target="_blank">scope management documentation</a> for an overview of supported scopes.</li>
+          </ul>
+        `,
+      },
+      "Data Store": {
+        description: `
+          Use FusionAuth as a comprehensive user data store, managed entirely through API calls to customize login flows, UX, and control user data storage.
+          <ul>
+            <li><strong>API-Driven Control</strong>: Utilize <a href="https://fusionauth.io/docs/apis/#overview" target="_blank">FusionAuth’s API capabilities</a> to manage user data, authentication, and authorization workflows entirely via API calls, integrating directly with your applications.</li>
+          </ul>
+        `,
+      }
   };
   
-  let answers = [];
   
+  
+  let answers = [];
+
   function displayQuestion(index) {
     const question = questions[index];
     const questionList = document.getElementById("question-list");
@@ -130,12 +194,11 @@ const questions = [
     selectedButton.classList.add("selected");
     otherButton.classList.remove("selected");
   
-    const next = questions[index][answer];
-  
-    // Clear all subsequent questions when a new answer is chosen
+    // Clear all subsequent answers and questions if the user changes their answer
     clearQuestionsFrom(index + 1);
   
-    // Display the next question or result
+    // Determine the next question or result
+    const next = questions[index][answer];
     if (typeof next === "string") {
       displayResult(next);
     } else {
@@ -146,10 +209,11 @@ const questions = [
   function clearQuestionsFrom(index) {
     const questionList = document.getElementById("question-list");
   
-    // Remove each question block starting from the given index
+    // Remove each question block and clear answers starting from the given index
     let nextQuestion = document.getElementById(`question-${index}`);
     while (nextQuestion) {
       questionList.removeChild(nextQuestion);
+      delete answers[index];
       index++;
       nextQuestion = document.getElementById(`question-${index}`);
     }
@@ -166,9 +230,7 @@ const questions = [
     resultText.innerHTML = `
       <h2>Your use case is: ${useCase}</h2>
       <p>${useCases[useCase].description}</p>
-      <h3>Suggested Diagrams:</h3>
-      <ul>${useCases[useCase].diagrams.map(diagram => `<li>${diagram}</li>`).join('')}</ul>
-      <p><a href="${useCases[useCase].documentationLink}" target="_blank">Read more in the documentation</a></p>
+      <p><a href="https://fusionauth.io/docs/get-started/use-cases" target="_blank">Explore more in-depth use case information here</a></p>
     `;
   }
   
